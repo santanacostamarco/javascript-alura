3b35e3526f8adf8a2b546247b372d42c91902dd4
Marco Antonio <santannacostamarco@gmail.com>
2018-02-07 09:49:52 -0200

    validate paciente

---
+++ js/validaPaciente.js
@@ -0,0 +1,17 @@
+function validaAltura(altura){
+  if (altura > 0 && altura < 3){
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+function validaPeso(peso){
+  if (peso > 0 && altura <= 1000){
+    return true;
+  } else {
+    return false;
+  }
+
+}
